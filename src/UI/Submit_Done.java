e8c34b803891a1c9034947ce56366f58b70ab444
PhoenixValued <78727906+PhoenixValued@users.noreply.github.com>
2022-05-21 08:36:34 +0800

    New files

--- src/UI/Submit_Done.java
+++ src/UI/Submit_Done.java
@@ -8,12 +8,12 @@ package UI;
  *
  * @author Kaita Armda
  */
-public class Login_Form_2 extends javax.swing.JFrame {
+public class Submit_Done extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login_Form_2
+     * Creates new form Submit_Done
      */
-    public Login_Form_2() {
+    public Submit_Done() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Login_Form_2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login_Form_2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Submit_Done.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login_Form_2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Submit_Done.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login_Form_2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Submit_Done.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login_Form_2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Submit_Done.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login_Form_2().setVisible(true);
+                new Submit_Done().setVisible(true);
             }
         });
     }
